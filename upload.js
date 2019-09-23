fd3fd615f152681d660dbcd35fe5c1642a50e9b8
Cory Nezin <cory.nezin@gmail.com>
2019-09-22 23:55:24 -0400

    update for lambda

--- upload.js
+++ upload.js
@@ -1,11 +1,15 @@
-const url = 'https://www.corynezin.com/transform'
-//const url = 'http://127.0.0.1:5000/transform'
+const url = 'https://8zbn4xqmwa.execute-api.us-east-2.amazonaws.com/default/opencv_layered'
 const form = document.querySelector('form')
+
+const toBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+});
 
 form.addEventListener('submit', e => {
   e.preventDefault()
-
-  const formData = new FormData();
 
 	let img = document.getElementById('img_file').files[0];
 	let txt = document.getElementById('txt_file').files[0];
@@ -14,33 +18,36 @@ form.addEventListener('submit', e => {
   let width = parseInt(document.getElementById('width').value)
   let negate = document.getElementById('negate').checked
 
-	formData.append('img_file', img);
-	formData.append('txt_file', txt);
-	formData.append('threshold', threshold);
-	formData.append('height', height);
-	formData.append('width', width);
-	formData.append('negate', negate);
+  const reader = new FileReader();
+  reader.readAsDataURL(img);
 
-  console.log('Posting to', url)
-
-  fetch(url, {
-    method: 'POST',
-    body: formData,
-  }).then(response => {
-    console.log(response)
-    const reader = response.body.getReader()
-    decoder = new TextDecoder("utf-8");
-    let result = ''
-    reader.read().then( function processText({ done, value }) {
-      if (done) {
-        console.log('done')
-        document.getElementById('main').innerHTML = result;
-        return
-      }
-      const chunk = value
-      console.log(chunk)
-      result += decoder.decode(chunk)
-      return reader.read().then(processText)
+  reader.onload = function(evt) {
+    let payload = {
+      image: evt.target.result.split(',')[1],
+      text: 'xyz',
+      threshold,
+      size: [height, width],
+      negate
+    }
+    console.log(payload)
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(payload),
+    }).then(response => {
+      console.log(response)
+      const reader = response.body.getReader()
+      decoder = new TextDecoder("utf-8");
+      let result = ''
+      reader.read().then( function processText({ done, value }) {
+        if (done) {
+          document.getElementById('main').innerHTML = JSON.parse(result)['chars'];
+          return
+        }
+        const chunk = value
+        console.log(chunk)
+        result += decoder.decode(chunk)
+        return reader.read().then(processText)
+      })
     })
-  })
+  }
 })
